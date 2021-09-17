c6f0460aafa970d6932c164018cb1a9551b40275
sayeeem <sayeemahmed1995@gmail.com>
2021-09-17 12:02:16 -0400

    Build KMM

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MarkAndSayeem"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/example/markandsayeem/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.markandsayeem
+
+expect class Platform() {
+    val platform: String
+}
