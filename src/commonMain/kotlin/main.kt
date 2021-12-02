61c91ab2e8c179858043e72051f5df0fd2111e70
markgaskin <markrgaskin@gmail.com>
2021-12-02 13:46:19 -0500

    Added 6x7 grid, logo, Score, and High Score to main view

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,14 +1,19 @@
 import com.soywiz.klock.*
 import com.soywiz.korge.*
+import com.soywiz.korge.html.*
 import com.soywiz.korge.tween.*
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.*
 import com.soywiz.korim.format.*
 import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.*
+import com.soywiz.korma.geom.vector.*
 import com.soywiz.korma.interpolation.*
+import com.soywiz.korim.font.*
+import com.soywiz.korim.text.TextAlignment
 
-suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
+suspend fun main() = Korge(width = 480, height = 640, title = "2048", bgcolor = RGBA(253, 247, 240)) {
+	/*
 	val minDegrees = (-16).degrees
 	val maxDegrees = (+16).degrees
 
@@ -19,8 +24,86 @@ suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"
 		position(256, 256)
 	}
 
-	while (true) {
+	while (false) {
 		image.tween(image::rotation[minDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
 		image.tween(image::rotation[maxDegrees], time = 1.seconds, easing = Easing.EASE_IN_OUT)
+	}
+	*/
+
+	val font = resourcesVfs["clear_sans.fnt"].readBitmapFont()
+
+	val gridColumns = 6
+	val gridRows = 7
+
+	val cellIndentSize = 8
+
+	val cellSize = views.virtualWidth / (gridColumns+2)
+	val fieldWidth = (cellIndentSize * (gridColumns+1)) + gridColumns * cellSize
+	val fieldHeight = (cellIndentSize * (gridRows+1)) + gridRows * cellSize
+	val leftIndent = (views.virtualWidth - fieldWidth) / 2
+	val topIndent = 155
+
+	val backgroundRect = roundRect(fieldWidth, fieldHeight, 5, fill = Colors["#b9aea0"]) {
+		position(leftIndent, topIndent)
+	}
+
+	graphics {
+		position(leftIndent, topIndent)
+		fill(Colors["#cec0b2"]) {
+			for (i in 0 until gridColumns) {
+				for (j in 0 until gridRows) {
+					roundRect(cellIndentSize + (cellIndentSize + cellSize) * i, cellIndentSize + (cellIndentSize + cellSize) * j, cellSize, cellSize, 5)
+				}
+			}
+		}
+	}
+
+	val bgLogo = roundRect(cellSize, cellSize, 5, fill = Colors["#6a00b0"]) {
+		position(leftIndent + cellIndentSize, 12)
+	}
+	text("Tr.io", cellSize * 0.5, Colors.WHITE, font).centerOn(bgLogo)
+
+	val bgBest = roundRect(cellSize * 2.5, cellSize * 1.5, 5.0, fill = Colors["#bbae9e"]) {
+		alignRightToRightOf(backgroundRect)
+		alignTopToTopOf(bgLogo,cellSize*0.5)
+	}
+	text("BEST", cellSize * 0.5, RGBA(239, 226, 210), font) {
+		centerXOn(bgBest)
+		alignTopToTopOf(bgBest, 3.0)
+	}
+	text("0", cellSize * 1.0, Colors.WHITE, font) {
+
+		setTextBounds(Rectangle(0.0, 0.0, bgBest.width, cellSize * 0.5))
+		alignment = TextAlignment.MIDDLE_CENTER
+		alignTopToTopOf(bgBest, 5.0 + (cellSize*0.5) + 5.0)
+		centerXOn(bgBest)
+	}
+
+	val bgScore = roundRect(cellSize * 2.5, cellSize*1.5, 5.0, fill = Colors["#bbae9e"]) {
+		alignRightToLeftOf(bgBest, 24.0)
+		alignTopToTopOf(bgBest)
+	}
+	text("SCORE", cellSize * 0.5, RGBA(239, 226, 210), font) {
+		centerXOn(bgScore)
+		alignTopToTopOf(bgScore, 3.0)
+	}
+	text(cellSize.toString(), cellSize * 1.0, Colors.WHITE, font) {
+		setTextBounds(Rectangle(0.0, 0.0, bgScore.width, cellSize * 0.5))
+		alignment = TextAlignment.MIDDLE_CENTER
+		centerXOn(bgScore)
+		alignTopToTopOf(bgScore, 5.0 + (cellSize*0.5) + 5.0)
+	}
+
+	val restartImg = resourcesVfs["restart.png"].readBitmap()
+
+	val btnSize = cellSize * 0.5
+	val restartBlock = container {
+		val background = roundRect(btnSize, btnSize, 5.0, fill = RGBA(185, 174, 160))
+		image(restartImg) {
+			size(btnSize * 0.8, btnSize * 0.8)
+			centerOn(background)
+		}
+		alignTopToBottomOf(bgLogo, 5)
+		alignRightToRightOf(bgLogo)
 	}
 }
